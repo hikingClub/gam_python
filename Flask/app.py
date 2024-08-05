6a506265c3ad2e73a91504ddb3033c6ac513c542
marioahn <89332401+marioahn@users.noreply.github.com>
2024-08-05 18:21:50 +0900

    1

--- Flask/app.py
+++ Flask/app.py
@@ -1,8 +1,11 @@
-from flask import Flask, request, jsonify, render_template
-from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
+from flask_cors import CORS
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
+CORS(app)  # 모든 도메인에서의 요청을 허용
 
 # 모델과 토크나이저 로드
 model_name = "skt/kogpt2-base-v2"
@@ -33,7 +36,6 @@ def autocomplete_search(query, max_length=7, num_return_sequences=5):
 @app.route('/')
 def home():
     return render_template('index.html')
-
 
 @app.route('/autocomplete', methods=['POST'])
 def autocomplete():
---
+++ Flask/readme.md
@@ -0,0 +1,4 @@
+### 설치 목록
+pip install flask, transformers
+pip install flask-cors
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
